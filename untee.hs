aa8ec71c4b14ac20e296f723255d4195b8d446ed
Tumi Steingrímsson <tumi.st@gmail.com>
2015-04-23 09:31:52 +0000

    Interpret filename - as stdin
    Added because of unfortunate behaviour of /dev/stdin on OS X.

--- untee.hs
+++ untee.hs
@@ -1,18 +1,22 @@
 import Control.Monad (forever, mapM_)
 import Control.Concurrent (forkIO)
-import System.IO (openFile, hGetLine, Handle, IOMode(..))
+import System.IO (openFile, hGetLine, Handle, IOMode(..), stdin)
 import System.Environment (getArgs)
 
 import Pipes.Concurrent
 import Pipes.Prelude
 import Pipes
 
-readPipe outbox fn = do
-  h <- openFile fn ReadMode
+getHandle :: String -> IO Handle
+getHandle "-" = return stdin
+getHandle fn  = openFile fn ReadMode
+
+forkReadPipe outbox fn = do
+  h <- getHandle fn
   forkIO $ runEffect $ fromHandle h >-> toOutput outbox
 
 main :: IO ()
 main = do
   (outbox, inbox) <- spawn Unbounded
-  getArgs >>= mapM_ (readPipe outbox)
+  getArgs >>= mapM_ (forkReadPipe outbox)
   runEffect $ fromInput inbox >-> stdoutLn
